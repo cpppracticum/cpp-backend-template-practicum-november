97a3ebc3f16262cd3dc12f5a1327899e8b9a6fec
Nester0v474 <misha2004nsk@gmail.com>
2025-09-26 11:42:44 +0700

    Update request_handler.h

--- sprint1/problems/map_json/precode/src/request_handler.h
+++ sprint1/problems/map_json/precode/src/request_handler.h
@@ -17,11 +17,20 @@ public:
 
     template <typename Body, typename Allocator, typename Send>
     void operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
-        // Обработать запрос request и отправить ответ, используя send
+        HandleApiRequest(std::forward<decltype(req)>(req), std::forward<Send>(send));
     }
 
 private:
+    template <typename Body, typename Allocator, typename Send>
+    void HandleApiRequest(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send);
+    
+    template <typename Body, typename Allocator, typename Send>
+    void HandleGetMapsList(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send);
+    
+    template <typename Body, typename Allocator, typename Send>
+    void HandleGetMap(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send);
+
     model::Game& game_;
 };
 
-}  // namespace http_handler
+} 
