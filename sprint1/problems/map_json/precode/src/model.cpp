7e6f1622c0c1c7f0413c71baba382b4879e9f5fd
Nester0v474 <misha2004nsk@gmail.com>
2025-09-28 23:37:20 +0700

    Update model.cpp

--- sprint1/problems/map_json/precode/src/model.cpp
+++ sprint1/problems/map_json/precode/src/model.cpp
@@ -6,16 +6,15 @@ namespace model {
 using namespace std::literals;
 
 void Map::AddOffice(Office office) {
-    if (warehouse_id_to_index_.contains(office.GetId())) {
-        throw std::invalid_argument("Duplicate warehouse");
+    if (office_id_to_index_.contains(office.GetId())) {  
+        throw std::invalid_argument("Duplicate office");  
     }
 
     const size_t index = offices_.size();
     Office& o = offices_.emplace_back(std::move(office));
     try {
-        warehouse_id_to_index_.emplace(o.GetId(), index);
+        office_id_to_index_.emplace(o.GetId(), index);  
     } catch (...) {
-        // Удаляем офис из вектора, если не удалось вставить в unordered_map
         offices_.pop_back();
         throw;
     }
